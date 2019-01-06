8b25d974ff5c5b7d6aba7a5adbb66d30523def26
hyryo513 <henry.heyi513@gmail.com>
2019-01-06 11:52:28 -0600

    first baselined version

---
+++ assets/javascript/game.js
@@ -0,0 +1,69 @@
+//global varaibles
+var answer;
+var wins = 0;
+var losses = 0;
+var value = [];
+var totalScore = 0;
+var resultMessage;
+
+//function
+function answerGenerator(){
+    answer = Math.floor(Math.random() * 101) + 19;
+};
+
+function valueGenerator(){
+    value = [];
+    for (i=0; i<4; i++){
+        value.push(Math.floor(Math.random() * 12) + 1);
+    }
+};
+
+function initializtion(){
+    answerGenerator();
+    valueGenerator();
+    totalScore = 0;
+    resultMessage = "";
+    $("#answer").text(answer);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    $("#totalScore").text(totalScore);
+    $("#crystal1").attr("data-crystalvalue", value[0]);
+    $("#crystal2").attr("data-crystalvalue", value[1]);
+    $("#crystal3").attr("data-crystalvalue", value[2]);
+    $("#crystal4").attr("data-crystalvalue", value[3]);
+};
+
+function resultCheck(){
+    if (totalScore === answer){
+        wins++;
+        resultMessage = "You Won!";
+        $("#resultMessage").text(resultMessage);
+        $("#wins").text(wins);
+        initializtion();
+    }
+    else if(totalScore > answer){
+        losses++;
+        resultMessage = "You Loss!";
+        $("#resultMessage").text(resultMessage);
+        $("#losses").text(losses);
+        initializtion();
+    }
+};
+
+function scoreCal(){
+    var crystalValue;
+    crystalValue = parseInt($(this).attr("data-crystalvalue"));
+    totalScore = totalScore + crystalValue;
+    $("#totalScore").text(totalScore);
+    resultCheck();
+};
+
+//script starts here
+initializtion();
+
+$("#crystal1").on("click", scoreCal);
+$("#crystal2").on("click", scoreCal);
+$("#crystal3").on("click", scoreCal);
+$("#crystal4").on("click", scoreCal);
+
+
